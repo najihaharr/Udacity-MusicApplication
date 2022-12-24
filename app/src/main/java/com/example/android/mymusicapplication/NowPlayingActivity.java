08d69648e09217e063537a69a12c59a41f577ac1
Najihah Ar <najihah.arr@gmail.com>
2022-12-25 03:07:04 +0800

    Task: Add comments to NowPlayingActivity.java

--- app/src/main/java/com/example/android/mymusicapplication/NowPlayingActivity.java
+++ app/src/main/java/com/example/android/mymusicapplication/NowPlayingActivity.java
@@ -14,11 +14,15 @@ public class NowPlayingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
 
+        // Get intent that was sent from various activity pages eg. Favourites, Rewind, Top10
         Intent intent = getIntent();
+
+        // Store the Extras sent from the activity pages into variables
         int albumPic = intent.getIntExtra("Album Art", 0);
         String songTitle = intent.getStringExtra("Song Title");
         String singer = intent.getStringExtra("Artiste");
 
+        // Find the Ids on the XML page to display the variables sent from intent
         ImageView albumArt = findViewById(R.id.nowplaying_art);
         TextView song = findViewById(R.id.nowplaying_song);
         TextView artiste = findViewById(R.id.nowplaying_artiste);
